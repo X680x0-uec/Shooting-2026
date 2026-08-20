d58a1f20646e31e457441f6d81e1a80842e42a79
wawon-arch <ryota.mt0831@icloud.com>
2026-08-20 18:09:02 +0900

    時止めを管理するスクリプト

---
+++ Assets/Scripts/Stop.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Stop : MonoBehaviour
+{
+    public bool _stop = false;
+    private float timer = 0;
+    public float stoptime = 5f;  //時間の停止時間
+    public InputAction stop;  //時止めのアクションを起こしたいキーの割り当てはインスペクターから
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        stop.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ここではdKey
+        if (stop.WasPressedThisFrame() && !_stop)
+        {
+            timer = 0;
+            _stop = true;
+        }
+        if (_stop)
+        {
+            timer += Time.deltaTime;
+            if(timer >= stoptime)
+            {
+                _stop = false;
+
+            }
+        }
+    }
+}
