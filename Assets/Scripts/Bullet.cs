7b19474f0201d1ece2329c3eeaf6a8c97c79bd1e
Uprairie <apurairi@gmail.com>
2026-09-07 17:39:34 +0900

    時間停止機能の追加（zkey）、時間停止時に撃った弾は速度がランダムに増減するように変更、弾の射出場所をInitialFireOffset変数で変更できるように調整

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,25 +3,41 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 10f; //弾の速さ
+    private bool IsMoving = true;
+    private float destroyTimer = 0;
 
     void Awake() {
-        Invoke("DestroyBullet", 1f); //生まれてから1秒後に消滅する
     }
 
     void Update() {
-        transform.Translate(0, speed * Time.deltaTime, 0); //常に上方向へ移動
-        
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, speed * Time.deltaTime); 
-        //進行方向に光線を飛ばすことで衝突判定。Colliderを使う実装と異なり、どれだけ速く弾を打ち出してもオブジェクトをすり抜けなくなる。
-        
-        if (hit.collider != null && hit.collider.CompareTag("Enemy")) {
-            //敵（Enemyタグのついたもの）に当たった時の処理
-            Destroy(gameObject);
+        if (IsMoving) {
+            transform.Translate(0, speed * Time.deltaTime, 0); //常に上方向へ移動
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, speed * Time.deltaTime);
+            //進行方向に光線を飛ばすことで衝突判定。Colliderを使う実装と異なり、どれだけ速く弾を打ち出してもオブジェクトをすり抜けなくなる。
+            destroyTimer += Time.deltaTime;
+            if (hit.collider != null && hit.collider.CompareTag("Enemy")) {
+                //敵（Enemyタグのついたもの）に当たった時の処理
+                Destroy(gameObject);
+            }
         }
+
+        if (destroyTimer > 1.0f) DestroyBullet();
     }
 
     private void DestroyBullet() {
         //弾削除時の処理を記述（爆発など）
         Destroy(gameObject);
+    }
+
+    public void Stop() {
+        IsMoving = false;
+    }
+
+    public void ChangeSpeed() {
+        speed *= Random.Range(0.8f, 4f);
+    }
+
+    public void Move() {
+        IsMoving = true;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,19 @@
+using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private bool IstimePassing = true;
     [SerializeField] private float moveSpeed = 1f; //自機の動く速さ
     Rigidbody2D rb; //Rigidbody2Dコンポーネントの取得用
-    [SerializeField] GameObject BulletPrefab; //弾のプレハブ（召喚できるオブジェクト）
+    [SerializeField] Bullet BulletPrefab; //弾のプレハブ（召喚できるオブジェクト）
     [SerializeField] private float bulletInterval = 0.1f; //弾を撃つ間隔
     private Coroutine shootLoop; //弾を継続的に発射するループ（コルーチン）の取得用
+    private List<Bullet> FiredBullets = new List<Bullet>();
+    [SerializeField] private Vector3 InitialFireOffset = Vector3.zero;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>(); //playerからRigidbody2Dコンポーネントを取得
@@ -29,9 +34,42 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void OnTimeStop(InputAction.CallbackContext callback) {
+        if (IstimePassing) {
+            TimeStop();
+        } else {
+            TimePass();
+        }
+    }
+
+    private void TimeStop() {
+        foreach (Bullet bullet in FiredBullets) {
+            IstimePassing = false;
+            bullet.Stop();
+        }
+    }
+
+    private void TimePass() {
+        foreach (Bullet bullet in FiredBullets) {
+            IstimePassing = true;
+            bullet.Move();
+        }
+    }
+
     private IEnumerator ShootLoop() {
         while (true) {
-            Instantiate(BulletPrefab, transform.position, Quaternion.identity); //BulletPrefabをオブジェクトの位置に初期角度で生成
+            Bullet bullet;
+            if (IstimePassing) {
+                Vector3 offset = InitialFireOffset;
+                bullet = Instantiate(BulletPrefab, transform.position + offset, Quaternion.identity);
+            } else {
+                Vector3 offset = InitialFireOffset * Random.Range(0.6f, 1.4f);
+                bullet = Instantiate(BulletPrefab, transform.position + offset, Quaternion.identity);
+                bullet.ChangeSpeed();
+                bullet.Stop();
+            }
+            FiredBullets.Add(bullet);
+            FiredBullets.RemoveAll(b => b == null);
             yield return new WaitForSeconds(bulletInterval); //bulletInterval秒待つ
         }
     }
